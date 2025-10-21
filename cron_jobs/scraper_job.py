156bc2a4c99e4bf96e6a50d48a88ec419a82c3c4
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-10-21 20:22:03 +0300

    Better logging 2.

--- cron_jobs/scraper_job.py
+++ cron_jobs/scraper_job.py
@@ -8,12 +8,12 @@ def run_job():
     run_scraper()
 
 
-def start_scraper_loop(interval_seconds: int = 1200, looped: bool = True):
+def start_scraper_loop(interval_seconds: int = 1200, is_looped: bool = True):
     def loop():
         while True:
             run_job()
             time.sleep(interval_seconds)
-            if not looped:
+            if not is_looped:
                 break
 
     threading.Thread(target=loop, daemon=True).start()
