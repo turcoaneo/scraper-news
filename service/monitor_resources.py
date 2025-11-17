e7a2c79d00e4a8db5f0a4dbadb79abf665f5f5bc
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-11-17 20:13:27 +0200

    Deployment. GitHub Action. Monitoring max.

--- service/monitor_resources.py
+++ service/monitor_resources.py
@@ -9,12 +9,16 @@ logger = get_logger()
 
 def monitor_resources(interval=10):
     process = psutil.Process()
+    mem_max = 0
+    cpu_max = 0
     while True:
         mem_mb = process.memory_info().rss / 1024 ** 2
         cpu_percent = process.cpu_percent(interval=1)
-        logger.warning(f"[RESOURCE] Memory: {mem_mb:.2f} MB | CPU: {cpu_percent:.2f}%")
+        if mem_mb > mem_max: mem_max = mem_mb
+        if cpu_percent > cpu_max: cpu_max = cpu_percent
+        logger.warning(f"[RESOURCE] Memory: {mem_mb:.2f}/{mem_max:.2f} (MB) | CPU: {cpu_percent:.2f}/{cpu_max:.2f}(%)")
         time.sleep(interval - 1)  # subtract the 1s used by cpu_percent
-        if mem_mb > 1500:
+        if mem_mb > 3500:
             logger.warning("Warning: Memory usage approaching ECS limit!")
 
 
