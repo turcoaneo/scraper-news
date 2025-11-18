1ea15bce3cb4175942def3c8bfb6b660818ff4e8
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-11-18 08:29:40 +0200

    Deployment. Fixing port.

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from app import create_app
 from app.utils.env_vars import APP_ENV
 from cron_jobs.scraper_job import start_scraper_loop
 from service.monitor_resources import monitor_resources
+from service.util.logger_util import get_logger
 
 app = create_app()
+logger = get_logger()
 
 if __name__ == "__main__":
+    exposed_port = 80 if APP_ENV in ["uat", "prod"] else 8000
+    logger.info(f"APP_ENV = {APP_ENV}, binding to port {exposed_port}")
+
     # Optional: start scraper loop alongside FastAPI
     start_scraper_loop(interval_sec=150, is_looped=True)
 
@@ -15,7 +20,5 @@ if __name__ == "__main__":
     monitor_resources()
 
     import uvicorn
-
-    exposed_port = 80 if APP_ENV in ["uat", "prod"] else 8000
 
     uvicorn.run(app, host="0.0.0.0", port=exposed_port)
