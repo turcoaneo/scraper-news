4aaa82bae9a58952c107dc0f1359a646deffdd2c
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2026-04-23 20:31:05 +0300

    Deployment - role.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -150,6 +150,7 @@ resource "aws_ecs_task_definition" "this" {
   memory                   = var.memory
 
   execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.scraper_task_role.arn
 
   container_definitions = jsonencode([
     {
@@ -187,6 +188,46 @@ resource "aws_ecs_task_definition" "this" {
     }
   ])
 }
+
+# -------------------------
+# IAM role and policy for S3 boto
+# -------------------------
+resource "aws_iam_role" "scraper_task_role" {
+  name = "scraper-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "scraper_task_role_s3" {
+  role = aws_iam_role.scraper_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject",
+        "s3:ListBucket"
+      ],
+      Resource = [
+        "arn:aws:s3:::scraper-storage-uat",
+        "arn:aws:s3:::scraper-storage-uat/*"
+      ]
+    }]
+  })
+}
+
 
 # -------------------------
 # ECS Service
