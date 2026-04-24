48a2b7e9a4a9a0c1d676ed4af266dc35473fc1e2
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2026-04-24 14:53:44 +0300

    Deployment - vpc.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,12 +10,13 @@ module "vpc" {
   name   = "scraper-news-vpc"
   cidr   = "10.0.0.0/16"
 
-  azs = ["eu-north-1a", "eu-north-1b"]
-
+  azs             = ["eu-north-1a", "eu-north-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 # -------------------------
